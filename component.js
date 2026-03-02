e0a1f566e7cb9a3157fa4ec80fae7b78081b9685
Andrea <andrea_1996_melli@hotmail.com>
2026-03-02 15:01:52 +0100

    Step 9: Component Configuration

---
+++ component.js
@@ -0,0 +1,37 @@
+sap.ui.define([
+   "sap/ui/core/UIComponent",
+   "sap/ui/model/json/JSONModel",
+   "sap/ui/model/resource/ResourceModel"
+], (UIComponent, JSONModel, ResourceModel) => {
+   "use strict";
+
+   return UIComponent.extend("ui5.walkthrough.Component", {
+
+      metadata : {
+         interfaces: ["sap.ui.core.IAsyncContentCreation"],
+         rootView: {
+            viewName: "ui5.walkthrough.view.App",
+            type: "XML",
+            id: "app"
+         }
+      },
+
+      init() {
+         // 1️⃣ Llamada a init del padre
+         UIComponent.prototype.init.apply(this, arguments);
+
+         // 2️⃣ Modelo de datos JSON
+         const oData = {
+            recipient: { name: "World" }
+         };
+         const oModel = new JSONModel(oData);
+         this.setModel(oModel);
+
+         // 3️⃣ Modelo i18n
+         const i18nModel = new ResourceModel({
+            bundleName: "ui5.walkthrough.i18n.i18n"
+         });
+         this.setModel(i18nModel, "i18n");
+      }
+   });
+});
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,41 +1,15 @@
 sap.ui.define([
    "sap/ui/core/mvc/Controller",
-   "sap/m/MessageToast",
-   "sap/ui/model/json/JSONModel",
-   "sap/ui/model/resource/ResourceModel"
-], (Controller, MessageToast, JSONModel, ResourceModel) => {
+   "sap/m/MessageToast"
+], (Controller, MessageToast) => {
    "use strict";
 
    return Controller.extend("ui5.walkthrough.controller.App", {
-
-     onInit() {
-         // 1️⃣ Modelo de datos JSON
-         const oData = {
-            recipient : { name : "World" }
-         };
-         const oModel = new JSONModel(oData);
-         this.getView().setModel(oModel);
-
-         // 2️⃣ Modelo de recursos (i18n)
-         const i18nModel = new ResourceModel({
-            bundleName: "ui5.walkthrough.i18n.i18n"
-         });
-         this.getView().setModel(i18nModel, "i18n");
-      },
-
       onShowHello() {
-         // 1️⃣ Acceder al ResourceBundle
          const oBundle = this.getView().getModel("i18n").getResourceBundle();
-
-         // 2️⃣ Leer valor del modelo JSON
          const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-
-         // 3️⃣ Reemplazar placeholder {0} en helloMsg
          const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-         // 4️⃣ Mostrar mensaje
          MessageToast.show(sMsg);
       }
-
    });
 });
