1d78d97d9ebf8f87a8ba18da858094303e562357
Andrea <andrea_1996_melli@hotmail.com>
2026-03-04 10:09:10 +0100

    Step 28: Integration Test with OPA

--- webapp/test/testsuite.qunit.js
+++ webapp/test/testsuite.qunit.js
@@ -18,8 +18,11 @@ sap.ui.define(() => {
         },
         tests: {
             "unit/unitTests": {
-                title: "UI5 Walkthrough - Unit Tests"
-            }
+				           title: "UI5 Walkthrough - Unit Tests"
+			        },
+			        "integration/opaTests": {
+				           title: "UI5 Walkthrough - Integration Tests"
+			        }
         }
     };
 });
